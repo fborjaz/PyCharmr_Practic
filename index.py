0f449dcf6fc8df984f4b4a425820189706a8c2d8
fborjaz <fborjaz@unemi.edu.ec>
2023-09-24 14:19:50 -0500

    Sube tood

---
+++ index.py
@@ -0,0 +1,13 @@
+# import random
+import random
+
+def Advina_el_num(x):
+    print("=============================================")
+    print("| ¡Bienvenido al juego de advina el numero! |")
+    print("=============================================")
+
+    print("Tu meta es adivinar el numero generado por la computadora")
+
+    Ln_num = random.randint(1, x) # numero aleatorio entre 1 y x.
+
+
