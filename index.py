1afbbbf7bc73d09579fa1847461cab3dc6ea8956
fborjaz <fborjaz@unemi.edu.ec>
2023-09-24 20:45:11 -0500

    Juego adivina el numero

--- index.py
+++ index.py
@@ -10,4 +10,16 @@ def Advina_el_num(x):
 
     Ln_num = random.randint(1, x) # numero aleatorio entre 1 y x.
 
+    prediction = 0
 
+    while prediction != Ln_num:
+        prediction = int(input(f"Ingresa un numero entre 1 y {x}: "))
+        if prediction < Ln_num:
+            print("Lo siento, el numero es muy bajo. Intenta de nuevo")
+        elif prediction > Ln_num:
+            print("Lo siento, el numero es muy alto. Intenta de nuevo")
+
+    print(f"¡Felicidades! Adivinaste el numero {Ln_num} correctamente")
+
+
+Advina_el_num(10)
