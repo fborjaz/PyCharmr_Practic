3ed94816212f363e3033e77eede4ae110a16b72a
fborjaz <fborjaz@unemi.edu.ec>
2023-09-25 22:34:24 -0500

    Exercise_1_resuelt

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,1 +1,11 @@
 # Crear un programa que le pida a un usuario una serie de numeros cualquiera y que solo dejaremos de hcaerlo cuando el usuario ingrese un numero igual a cero.
+
+Ln_num = int(input("Ingresa un numero: "))
+while Ln_num != 0:
+    Ln_num = int(input("Ingresa otro numero: "))
+    if Ln_num == 0:
+        break
+    else:
+        continue
+
+print("¡Felicidades! Adivinaste el numero 0 correctamente")
