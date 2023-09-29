68ed8e15878e28cfd13d225b4bfd26ed4801cdd0
fborjaz <fborjaz@unemi.edu.ec>
2023-09-28 22:21:24 -0500

    Exercise_4

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,11 +1,11 @@
 # Leer un número entero positivo desde teclado e imprimir la suma de los dígitos que lo componen
 
-num_str = input("Ingresa un número entero positivo: ")
+Ln_num_str = input("Ingresa un número entero positivo: ")
 
 # Verificar si la entrada es un número entero positivo
-if num_str.isdigit():
-    num = int(num_str)
-    digit_sum = sum(int(digit) for digit in num_str)
-    print("La suma de los dígitos es:", digit_sum)
+if Ln_num_str.isdigit():
+    num = int(Ln_num_str)
+    Ln_digit_sum = sum(int(Ln_digit) for Ln_digit in Ln_num_str)
+    print("La suma de los dígitos es:", Ln_digit_sum)
 else:
     print("Por favor, ingresa un número entero positivo válido.")
