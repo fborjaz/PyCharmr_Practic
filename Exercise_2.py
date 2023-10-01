e8a4b3a5ce0156ded0695e73851f60eb191037a5
fborjaz <fborjaz@unemi.edu.ec>
2023-10-01 13:16:14 -0500

    Exercise_2 update

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,14 +1,12 @@
 # Ingrese un numero positivo y muestrelo, caso contrario solicite nuevamente el numero
 
-Ln_num = int(input("Ingrese un numero: "))
-
-if Ln_num > 0:
-    print(f"El numero ingresado fue: {Ln_num}")
-
-while Ln_num < 0:
-    Ln_num = int(input("Ingrese un numero positivo: "))
-    if Ln_num > 0:
-        print(f"El numero ingresado fue: {Ln_num}")
-        break
-    else:
-        continue
+while True:
+    try:
+        Ln_num = int(input('Ingrese un numero: '))
+        if Ln_num > 0:
+            print(f'El numero ingresado fue: {Ln_num}')
+            break
+        else:
+            print('Error: Debe ingresar un numero positivo.')
+    except ValueError:
+        print('Error: Ingresar solo numeros')
